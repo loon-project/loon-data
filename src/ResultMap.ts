82daf2d8df6a8c9e544c0c2e13e930356b9f2d31
vincent178 <vincent.007.cn@gmail.com>
2017-05-04 22:10:40 +0800

    update

--- src/ResultMap.ts
+++ src/ResultMap.ts
@@ -1,7 +1,42 @@
+import {Result} from "./Result";
 
-export function ResultsMap() {
+interface Klass {
+    new (...args): any;
+    name: string;
+}
+
+
+export function ResultsMap(result: Result) {
 
     return (target: any, key: string, descriptor) => {
+
+        const method = descriptor.value;
+
+        descriptor.value = async (...args) => {
+
+            const type = <Klass> result.type;
+
+            const ret = await method.apply(target, args);
+
+            if (Array.isArray(ret)) {
+
+                return ret.map(item => {
+
+                    const ins = new type();
+
+                    Object.keys(item).forEach(key => {
+                        if (item.hasOwnProperty(key)) {
+                            ins[key] = item[key];
+                        }
+                    });
+
+                    return ins;
+
+                });
+            }
+
+
+        };
 
     };
 }
