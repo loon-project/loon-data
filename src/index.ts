a6f21c8f16cc9197bb548331bd8fb59ba5fde92d
vincent178 <vincent.007.cn@gmail.com>
2017-05-07 05:32:35 +0800

    export content

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import * as Knex from "knex";
 
-
-const client = Knex({
-    client: 'mysql',
-    connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'lion_development',
-        timezone: 'utc'
-    },
-    pool: {
-        min: 10,
-        max: 10
-    }
-});
-
-
-client.from('users').limit(1).then(data => {
-    console.log(Array.isArray(data));
-    console.log(data);
-});
-
+export * from './DataMap';
+export * from './ResultMap';
